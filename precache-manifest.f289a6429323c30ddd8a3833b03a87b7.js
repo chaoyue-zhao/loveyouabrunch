b603e830e63940db1efca6d68cfd2f75e892a40e
chaoyue-zhao <chaoyue.zhao.2011@gmail.com>
2019-01-20 15:01:17 -0500

    Updates

---
+++ precache-manifest.f289a6429323c30ddd8a3833b03a87b7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f393a23660631ffe591",
+    "url": "/loveyouabrunch/static/css/main.fa0b6599.chunk.css"
+  },
+  {
+    "revision": "9f393a23660631ffe591",
+    "url": "/loveyouabrunch/static/js/main.9f393a23.chunk.js"
+  },
+  {
+    "revision": "3e6801ce8131a97ecbdb",
+    "url": "/loveyouabrunch/static/js/1.3e6801ce.chunk.js"
+  },
+  {
+    "revision": "fe809045f93ff2f7d948",
+    "url": "/loveyouabrunch/static/js/runtime~main.fe809045.js"
+  },
+  {
+    "revision": "460f07e9285763b040dcdaf2cb230cec",
+    "url": "/loveyouabrunch/static/media/bgImg.460f07e9.jpg"
+  },
+  {
+    "revision": "ed668bf0b123371be46d005b428f6188",
+    "url": "/loveyouabrunch/static/media/owfont-regular.ed668bf0.woff"
+  },
+  {
+    "revision": "e00c269cd4fb37b24eea2e4f37982c3d",
+    "url": "/loveyouabrunch/static/media/owfont-regular.e00c269c.eot"
+  },
+  {
+    "revision": "3d73387c2a1e21f207a8357724f536ba",
+    "url": "/loveyouabrunch/static/media/owfont-regular.3d73387c.ttf"
+  },
+  {
+    "revision": "f3ce0ef136eaaf2b1db22cf4069cb5dc",
+    "url": "/loveyouabrunch/static/media/owfont-regular.f3ce0ef1.svg"
+  },
+  {
+    "revision": "9ea57640dddc27ac8e1925e9f9ae27a7",
+    "url": "/loveyouabrunch/static/media/Yelp_trademark_RGB.9ea57640.png"
+  },
+  {
+    "revision": "0b192de7c6119c111b87369444866aaa",
+    "url": "/loveyouabrunch/index.html"
+  }
+];
